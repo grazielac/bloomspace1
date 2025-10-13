41710ab1e15578861f84856da5034453617649ed
grazielac <cpgraziela@gmail.com>
2025-10-13 20:34:24 +0100

    Create a basic server: testing

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pg from "pg";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "BloomSpace backend is running!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http:??localhost:${PORT} `);
+});
