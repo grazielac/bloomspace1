dcafdb1e99560bfbf76af27e04a3862970137d4f
grazielac <cpgraziela@gmail.com>
2025-10-14 21:30:22 +0100

    Style header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,25 @@
 export default function Header() {
-    return (
-        <>
-            <header>
-                <h1>Bluméa</h1>
-                <div>
-                    <img src="" alt="" />
-                </div>
-                <nav>
-                    <search>
-                        <form action="./search/">
-                            <label for="search">Search</label>
-                            <input type="search" id="search" name="search" />
-                            <button type="submit">Search</button>
-                        </form>
-                    </search>
-                    <button>Sign in</button>
-                    <button>Register</button>
-                </nav>
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header className="p-4 border-b">
+        <div className="flex justify-between items-center cursor-pointer">
+          <h1 className="text-2xl font-bold">Bluméa</h1>
+          <div>
+            <img src="" alt="" />
+          </div>
+          {/* Search + buttons */}
+          <div className="flex items-center space-x-4">
+            <form action="./search/" className="flex items-center space-x-2 cursor-pointer">
+              <label htmlFor="search" className="sr-only cursor-pointer">Search</label>
+              <input type="search" id="search" name="search" placeholder="Search..." className="border rounded-3xl px-2 py-1"/>
+              <button type="submit" className="bg-gray-200 px-3 py-1 rounded-3xl cursor-pointer">Search</button>
+            </form>
+
+            <button className="cursor-pointer">Sign in</button>
+            <button className="px-3 py-1 rounded-3xl bg-gray-500 text-white cursor-pointer">Register</button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+    return (
+        <>
+        <div className="h-screen bg-amber-100">
+            <div className="h-full border">
+
+            </div>
+        </div>
+        </>
+    )
+}
