ffed866662b2de628ae8d25b77e3f39309cbffe5
grazielac <cpgraziela@gmail.com>
2025-10-18 12:34:31 +0100

    Add array of images as placeholder

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,23 +1,51 @@
 export default function Feed() {
+  const events = [
+    {
+      name: "Event 1",
+      date: "18 Oct 2025",
+      desc: "Description 1",
+      img: "/src/assets/sample.jpg",
+    },
+    {
+      name: "Event 2",
+      date: "19 Oct 2025",
+      desc: "Description 2",
+      img: "/src/assets/sample.jpg",
+    },
+    {
+      name: "Event 3",
+      date: "20 Oct 2025",
+      desc: "Description 3",
+      img: "/src/assets/sample.jpg",
+    },
+  ];
   return (
     <>
-      <div className="p-4 md:0-8">
-        <h1 className="text-2xl font-bold mb-6">Upcoming Events</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="p-4 md:p-8 flex flex-col md:flex-row items-start gap-4">
+        <div className="flex-1">
+          <div className="flex justify-between mb-4">
+            <h1 className="text-2xl font-bold mb-6">Upcoming Events</h1>
+            <button className="bg-gray-300 p-4 rounded-3xl self-start">
+              Create
+            </button>
+          </div>
           {/* EVENT CARD */}
-          <div className="bg-white rounded-xl shadow p-4">
-            <div className="w-1/2">
-              <img
-                src="/src/assets/sample.jpg"
-                alt="Event"
-                className="w-full h-auto rounded-lg mb-4"
-              />
-            </div>
-            <h2 className="font-bold text-lg mb-1">Event Name</h2>
-            <p className="text-sm text-gray-500 mb-2">Date: 18 Oct 2025</p>
-            <p className="text-gray-700 text-sm line-clamp-3">
-              Short description of the event
-            </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {events.map((event, photo) => (
+              <div key={photo} className="bg-white rounded-xl shadow p-4">
+                <img
+                  src={event.img}
+                  alt={event.alt}
+                  className="w-full h-auto rounded-lg mb-4"
+                />
+
+                <h2 className="font-bold text-lg mb-1">{event.name}</h2>
+                <p className="text-sm text-gray-500 mb-2">{event.date}</p>
+                <p className="text-gray-700 text-sm line-clamp-3">
+                  {event.desc}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
