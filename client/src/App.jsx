f0f3d77fc1691bc1a49cc9ed10de24e1de0b026b
grazielac <cpgraziela@gmail.com>
2025-10-18 12:05:43 +0100

    Add event card on Feed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import RegisterForm from "./components/RegisterForm";
+import Feed from "./components/Feed";
 
 
 function App() {
   return (
     <>
       <Header />
+      <Feed />
       <Hero />
       <Footer />
       
--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,6 +1,26 @@
 export default function Feed() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <>
+      <div className="p-4 md:0-8">
+        <h1 className="text-2xl font-bold mb-6">Upcoming Events</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {/* EVENT CARD */}
+          <div className="bg-white rounded-xl shadow p-4">
+            <div className="w-1/2">
+              <img
+                src="/src/assets/sample.jpg"
+                alt="Event"
+                className="w-full h-auto rounded-lg mb-4"
+              />
+            </div>
+            <h2 className="font-bold text-lg mb-1">Event Name</h2>
+            <p className="text-sm text-gray-500 mb-2">Date: 18 Oct 2025</p>
+            <p className="text-gray-700 text-sm line-clamp-3">
+              Short description of the event
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
